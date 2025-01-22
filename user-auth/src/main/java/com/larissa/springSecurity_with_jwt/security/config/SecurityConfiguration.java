d5159169ba1314f3efd4e98fea67842bc9249c2f
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2025-01-22 18:29:41 -0300

    Permissions defined

--- user-auth/src/main/java/com/larissa/springSecurity_with_jwt/security/config/SecurityConfiguration.java
+++ user-auth/src/main/java/com/larissa/springSecurity_with_jwt/security/config/SecurityConfiguration.java
@@ -28,13 +28,27 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/favoritos").hasRole("ADMIN")
-                                .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/movies").hasRole("admin")
+                        .requestMatchers(HttpMethod.DELETE, "/movies/{id}").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET, "/movies").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/movies/{id}").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/sessions").hasRole("admin")
+                        .requestMatchers(HttpMethod.DELETE, "/sessions/{id}").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET, "/sessions").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/sessions/{id}").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/tickets").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET, "/tickets").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET, "/tickets/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/tickets/session/{sessionId}").permitAll()
+
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .cors().and()
                 .build();
     }
 
